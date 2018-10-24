0a01734a91ed26f9bbe737421865b43d3bf853e4
julenb <21701719@live.uem.es>
2018-10-24 20:51:36 +0200

    Logo app

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -10,8 +10,9 @@ public class Main extends Application {
     private AnchorPane myPane;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         myPane = FXMLLoader.load(getClass().getResource("view/cchannel.fxml"));
+        primaryStage.getIcons().add(new Image("file:src/img/CChannel.png"));
         primaryStage.setTitle("CChannel");
         primaryStage.setScene(new Scene(myPane));
         primaryStage.show();
