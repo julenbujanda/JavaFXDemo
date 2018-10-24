04b93a4e111eeb2b92f515e7872869da1f4707d4
julenb <21701719@live.uem.es>
2018-10-24 16:46:50 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private AnchorPane myPane;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        myPane = FXMLLoader.load(getClass().getResource("../view/cchannel.fxml"));
+        primaryStage.setTitle("CChannel");
+        primaryStage.setScene(new Scene(myPane));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
